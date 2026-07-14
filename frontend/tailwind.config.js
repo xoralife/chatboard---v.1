75b8c15335c8e6eda648221300001d03438620e8
xoralife <abidhussainiouu@gmail.com>
2026-07-14 14:58:14 +0500

    feat: add tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
